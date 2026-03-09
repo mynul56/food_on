cbb982ab201b93c25f532c03a3ce68911a83c4f2
mynul56 <mynulislamtanim@gmail.com>
2026-03-09 15:19:42 +0600

    Update API URL in AppConstants for local development

--- frontend/lib/core/utils/constants.dart
+++ frontend/lib/core/utils/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String apiUrl = 'http://localhost:5000/api';
+  static const String apiUrl = 'http://10.0.2.2:5000/api';
   static const String tokenKey = 'token';
   static const String userKey = 'user';
 }
